8d7dad49d8e7c0cad6dc0cc597a32eb5632306db
TeamInnexia <81477304+TeamInnexia@users.noreply.github.com>
2021-07-29 01:05:09 +0530

    Initial commit

---
+++ web/index.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+     <meta charset="UTF-8">
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+     <script src="https://kit.fontawesome.com/a81368914c.js"></script>
+     <link href="https://fonts.googleapis.com/css2?family=Cutive+Mono&family=Poppins:wght@100;200;300;400;500;600;700&display=swap"
+          rel="stylesheet">
+     <link rel="stylesheet" href="src/style.css">
+     <title>SAMMY (@useIes)</title>
+     <link rel="icon" href="https://telegra.ph/file/8e58f06b85c35b1932b8a.jpg" type="image/x-icon">
+</head>
+
+<body id="body" class="dark">
+     <div class="wrapper">
+          <div class="toggle">
+               <input class="toggle-input" type="checkbox" />
+               <div class="toggle-bg"></div>
+               <div class="toggle-switch">
+                    <div class="toggle-switch-figure"></div>
+                    <div class="toggle-switch-figureAlt"></div>
+               </div>
+          </div>
+     </div>
+     <div class="modal">
+          <img src="https://telegra.ph/file/46be66f3b2514e5cd017e.jpg" alt="">
+          <div class="close"></div>
+     </div>
+
+     <div id="container" class="container">
+          <input type="checkbox" name="" id="check">
+          <div class="card">
+               <div class="sidebar" id="sidebar">
+                    <ul>
+                         <li><a class="active" href="https://telegram.dog/useIes">Telegram</a></li>
+                         <li><a class="active" href="https://telegram.dog/SiderzBot">Robot</a></li>
+                    </ul>
+               </div>
+               <div class="header">
+                    <label for="check" class="checkbtn" id="chkbtn">
+                         <div class="hamburger-menu">
+                              <div class="center"></div>
+                         </div>
+                    </label>
+                    <a href="mailto:mrsammyxd@gmail.com" class="mail">
+                         <i class="far fa-envelope"></i>
+                    </a>
+                    <div class="main">
+                         <div class="image">
+                              <div class="hover">
+                                   <i class="fas fa-camera fa-2x"></i>
+                              </div>
+                         </div>
+                         <h3 class="name">SAMMY</h3>
+                         <h3 class="sub-name">@SiderzBot</h3>
+                    </div>
+               </div>
+
+               <div class="content">
+                    <div class="left">
+                         <div class="about-container">
+                              <h3 class="title">About</h3>
+                              <p class="text">🔭 I'ᴍ Cᴜʀʀᴇɴᴛʟʏ Wᴏʀᴋɪɴɢ Oɴ Iɴɴᴇxɪᴀ Bᴏᴛ | 🌱 I'ᴍ Cᴜʀʀᴇɴᴛʟʏ Lᴇᴀʀɴɪɴɢ Pʏᴛʜᴏɴ Aɴᴅ Hᴀᴄᴋɪɴɢ| 🤔 I'ᴍ Lᴏᴏᴋɪɴɢ Fᴏʀ Hᴇʟᴘ Wɪᴛʜ Nᴏᴏʙ Cᴏᴅᴇʀ | ⚡ Fᴜɴ Fᴀᴄᴛ I Aᴍ Nᴏᴏʙ </p>
+                         </div>
+                         <div class="icons-container">
+                         <a href="https://github.com/DarkCybers" class="icon">
+                                   <i class="fab fa-github"></i>
+                              </a>
+                              <a href="https://telegram.dog/SiderzBot" class="icon">
+                                   <i class="fab fa-telegram"></i>
+                              </a>
+                              <a href="                              https://www.youtube.com/channel/UCJEMDBgp_zYywtotOBrYJYw" class="icon">
+                                   <i class="fab fa-youtube"></i>
+                              </a>
+                         </div>
+                         <div class="buttons-wrap">
+                             
+            
+                         </div>
+                    </div>
+               </div>
+          </div>
+     </div>
+    
+            <script src="https://cdn.jsdelivr.net/gh/cachecleanerjeet/contact-form@master/src/contact-form.min.js" id="contactform" form_worker_url="https://quiet-art-65f4.kunar1318.workers.dev/"></script>
+
+     <script src="src/app.js"></script>
+</body>
+
+</html>
